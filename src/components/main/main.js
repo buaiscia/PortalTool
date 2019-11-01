bfdaa03962b12b1c417341963be4580262fa1c1b
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2019-11-01 15:07:40 +0100

    add preview + iframe

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,10 +1,41 @@
-import React from "react"
+import React from "react";
+import Iframe from 'react-iframe';
+import PKI from '../../images/PKI.png';
+import mob_mon from '../../images/mob_mon.png';
+import classes from "./Main.module.css"
 
-const Main = (props) => {
+const MainPage = (props) => {
     return(
-            <p>1244</p>
+        <div id="main">
+            <div id="top">
+                        {/* <a href="http://127.0.0.1:8000"><h1>Portal Tool Homepage</h1></a> */}
+                        <p><a href="#" onClick={(e) => props.closeFrame(e)}>Close the tab</a></p>
+                        {/* <p><a href="http://127.0.0.1:8000">Close the tab</a></p> */}
+            </div>
+
+            <div id="imgGrid" className={classes.imgGrid}>
+
+                            <a href="#" onClick={(e) => props.openFrame(e, 1)}><img src={PKI} alt="PKI Deployment"></img></a> 
+                        
+                            <a href="#" onClick={(e) => props.openFrame(e, 2)}><img src={mob_mon} alt="Mobile Monitoring"></img></a>
+
+            </div>
+
+                                   
+            <div id="bigFrameDiv">
+                        <Iframe 
+                            width="1500px"
+                            height="900px"
+                            id="theFrame"
+                            name="theFrame"
+                            display="initial"
+                            position="relative"
+                            target="_self"/>
+
+            </div>
+        </div>
 
     )
 }
 
-export default Main
+export default MainPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Layout from "../components/layout"
 // import SEO from "../components/seo"
 import Sidebar from "../components/sidebar/Sidebar"
 // import TechTag from "../components/tags/TechTag"
-import Main from "../components/main/main"
+import MainPage from "../components/main/Main"
 
 const IndexPage = ({ data }) => {
   // const posts = data.allMarkdownRemark.edges
@@ -36,37 +36,8 @@ const IndexPage = ({ data }) => {
           <Sidebar />
         </div>
         <div className="post-list-main">
-             <Main />
-          {/* {posts.map((post) => {
-            const tags = post.node.frontmatter.tags
-            return (
-              <div key={post.node.id} className="container mt-5">
-                <Link
-                  to={post.node.fields.slug}
-                  className="text-dark"
-                >
-                  <h2 className="title">{post.node.frontmatter.title}</h2>
-                </Link>
-                <small className="d-block text-info"><i>Posted on {post.node.frontmatter.date}</i>
-                </small>
-                <p className="mt-3 d-inline">{post.node.excerpt}</p>
-                <Link
-                  to={post.node.fields.slug}
-                  className="text-primary"
-                >
-                  <small className="d-inline-block ml-3"> Read full post</small>
-                </Link>
-                <div className="d-block">
-                  {getTechTags(tags)}
-                </div>
-              </div>
-            )
-          })} */}
-          {/* <div className="mt-4 text-center">
-            <Link to={nextPage} rel="next" style={{ textDecoration: `none` }}>
-              <span className="text-dark">Next Page →</span>
-            </Link>
-          </div> */}
+             <MainPage />
+          
         </div>
       </div>
     </Layout>
